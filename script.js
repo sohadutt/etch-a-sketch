2dc55459530128c9351f2704addacf72fcba3ded
sohadutt <sohadutt@users.noreply.github.com>
2025-06-15 12:42:10 +0530

    basic funtionality done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 const container = document.getElementById('sketch-container');
+
+
+for (let i = 0; i < 10000; i++) {
+    const sketch = document.createElement('div');
+    sketch.className = 'canvas';
+    
+    
+    // Add hover effect
+    sketch.addEventListener('mouseover', () => {
+        sketch.style.backgroundColor = 'blue';
+    });
+    container.appendChild(sketch);
+  
+}
+
+const clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', () => {
+    const canvases = document.querySelectorAll('.canvas');
+    canvases.forEach(canvas => {
+        canvas.style.backgroundColor = 'lightblue';
+    });
+});

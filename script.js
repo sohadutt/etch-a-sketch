33bb5128f87b0830ff88b468a779e36bb6cc1324
sohadutt <sohadutt@users.noreply.github.com>
2025-06-15 13:15:24 +0530

    added lot of new features

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const container = document.getElementById('sketch-container');
-
+let inkColor = 'blue';
+const blueButton = document.getElementById('blue-ink-button');
+const eraserButton = document.getElementById('eraser-button');
+const rainbowButton = document.getElementById('rainbow-ink-button');
 
 for (let i = 0; i < 10000; i++) {
     const sketch = document.createElement('div');
     sketch.className = 'canvas';
-    
-    
-    // Add hover effect
     sketch.addEventListener('mouseover', () => {
-        sketch.style.backgroundColor = 'blue';
+        if (inkColor === 'rainbow') {
+            sketch.style.backgroundColor = rainbowInk();
+        } else {
+            sketch.style.backgroundColor = inkColor;
+        }
     });
     container.appendChild(sketch);
-  
 }
+
+const blackButton = document.getElementById('black-ink-button');
+blackButton.addEventListener('click', () => {
+    inkColor = 'black';
+});
+
+blueButton.addEventListener('click', () => {
+    inkColor = 'blue';
+});
+
+eraserButton.addEventListener('click', () => {
+    inkColor = 'lightblue';
+});
+
+rainbowButton.addEventListener('click', () => {
+    inkColor = 'rainbow';
+});
 
 const clearButton = document.getElementById('clear-button');
 clearButton.addEventListener('click', () => {
@@ -21,3 +41,8 @@ clearButton.addEventListener('click', () => {
         canvas.style.backgroundColor = 'lightblue';
     });
 });
+
+function rainbowInk() {
+    const rainbow = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    return rainbow;
+}
